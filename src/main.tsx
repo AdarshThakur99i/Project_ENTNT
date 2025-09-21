eb6d02972fcdfcdeac22dbaf07596283b70163af
AdarshThakur398 <nutius90@gmail.com>
2025-09-21 10:49:19 +0530

    production setup for msw

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,7 @@ import App from './App.tsx';
 
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
+
   
  
   const { worker } = await import('./data/database/browser');
